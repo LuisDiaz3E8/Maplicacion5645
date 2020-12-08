0dd82a08546c13c6859d9d19f3fe0743e4453bac
complot49 <60439501+complot49@users.noreply.github.com>
2020-12-07 20:01:50 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import *
+root = Tk()
+def fun1():
+   root.destroy()
+   root2=Tk()
+  #add buttons, categories here for movies
+
+def fun2():
+   root.destroy()
+   root3=Tk()
+  #add buttons, categories here for shows
+
+
+
+bt1 = Button(root, text = 'movies', command=fun1)
+bt2 = Button(root, text = 'shows', command=fun2)
+
+bt1.pack()
+bt2.pack()
+
+root.mainloop()
